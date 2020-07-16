01e09ee9f9d26c86736044955e3334431e96f454
amorphist <azad.mamiyev.98@gmail.com>
2020-07-16 23:46:04 +0200

    app class and timber installation

---
+++ app/src/main/java/az/siftoshka/cubemate/App.kt
@@ -0,0 +1,16 @@
+package az.siftoshka.cubemate
+
+import android.app.Application
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
+    }
+}
--- app/src/main/java/az/siftoshka/cubemate/MainActivity.kt
+++ app/src/main/java/az/siftoshka/cubemate/MainActivity.kt
@@ -1,7 +1,7 @@
 package az.siftoshka.cubemate
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
