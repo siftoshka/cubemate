b1666c2315297202594eb34cd758e385a218fb4c
amorphist <azad.mamiyev.98@gmail.com>
2020-07-16 16:40:01 +0200

    init

---
+++ app/src/main/java/az/siftoshka/cubemate/MainActivity.kt
@@ -0,0 +1,11 @@
+package az.siftoshka.cubemate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
