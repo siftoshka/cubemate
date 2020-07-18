8f1fbf54de673ae199d21bfb11d2fcadc1f5262a
amorphist <azad.mamiyev.98@gmail.com>
2020-07-18 17:06:04 +0200

    ui designing

--- app/src/main/java/az/siftoshka/cubemate/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/az/siftoshka/cubemate/ui/fragments/SettingsFragment.kt
@@ -1,8 +1,16 @@
 package az.siftoshka.cubemate.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import az.siftoshka.cubemate.R
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 class SettingsFragment: Fragment(R.layout.fragment_settings) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        cToolbar.setExpandedTitleTextAppearance(R.style.CollapsingExpanded)
+        cToolbar.setCollapsedTitleTextAppearance(R.style.CollapsingCollapsed)
+    }
 
 }
--- app/src/main/java/az/siftoshka/cubemate/ui/fragments/StatisticsFragment.kt
+++ app/src/main/java/az/siftoshka/cubemate/ui/fragments/StatisticsFragment.kt
@@ -1,14 +1,22 @@
 package az.siftoshka.cubemate.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import az.siftoshka.cubemate.R
 import az.siftoshka.cubemate.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 @AndroidEntryPoint
 class StatisticsFragment: Fragment(R.layout.fragment_statistics) {
 
     private val viewModel: MainViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        cToolbar.setExpandedTitleTextAppearance(R.style.CollapsingExpanded)
+        cToolbar.setCollapsedTitleTextAppearance(R.style.CollapsingCollapsed)
+    }
 
 }
